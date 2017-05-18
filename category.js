16c3e70bf8e77fdfac5cae93cd2a7352b83a05f7
Mshkeso <msh.keso@gmail.com>
2017-05-18 09:39:23 +0200

    US7T11 and T12

---
+++ category.js
@@ -0,0 +1,8 @@
+module.exports = class Category {
+// An item that could be added to a shopping list.
+// The item can have a name, a quantity and a category.
+
+	constructor(name) {
+		this.name = name;
+	}
+}
