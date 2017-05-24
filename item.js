56fc8d29c0379fa973af445a4fb7bb2dfc0f612b
Mshkeso <msh.keso@gmail.com>
2017-05-24 15:52:05 +0200

    added test click as reference

---
+++ item.js
@@ -0,0 +1,63 @@
+class Item {
+// An item that could be added to a shopping list.
+// The item can have a name, a quantity and a category.
+
+	constructor(name, quantity, category) {
+		this.name = name;
+		this._quantity = quantity;
+		this.category = category;
+		this.bought = false;
+	}
+
+	set quantity(val){
+		if (val >= 0) {
+			this._quantity = val;
+		}
+		else {
+			this._quantity = 0;
+		}
+	}
+
+	get quantity(){
+		return this._quantity;
+	}
+
+	set category(val){
+		if(typeof val != "string" || val.length < 0){
+			throw(new Error("Bad name!"));
+		}
+		this._category = val;
+	}
+
+	get category(){
+		return this._category;
+	}
+
+	set bought(val){
+		if(typeof val != "boolean"){
+			throw(new Error("No boolean!"));
+		}
+		this._bought = val;
+	}
+
+	get bought(){
+		return this._bought;
+	}
+
+	set name(name){
+		this._name = name;
+	}
+
+	get name(){
+		return this._name;
+	}
+
+	addQuantity() {
+		this.quantity++;
+	}
+
+}
+
+if(typeof module !== "undefined"){
+	module.exports = Item;
+}
---
+++ shopping-list-collection.js
@@ -0,0 +1,29 @@
+if(typeof module !== "undefined"){
+	ShoppingList = require('./shopping-list.js');
+}
+
+class ShoppingListCollection {
+// A collection of shopping lists
+
+	constructor() {
+		this.shoppingLists = [];
+	}
+	
+	addShoppingList(shoppingList) {
+	// add a shoppinglist to the collection of shoppingLists
+
+		this.shoppingLists.push(shoppingList);
+	}
+
+	deleteShoppingList(name) {
+		for(let i = 0; i < this.shoppingLists.length; i++){
+			if(this.shoppingLists[i].name === name){
+				this.shoppingLists.splice(i, 1);
+			}
+		}
+	}
+}
+
+if(typeof module !== "undefined"){
+	module.exports = ShoppingListCollection;
+}
