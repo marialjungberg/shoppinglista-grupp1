0f8376bb1e980ceab1c18beb488423658e6de614
Mshkeso <msh.keso@gmail.com>
2017-05-30 13:54:16 +0200

    added functionallity for edit list name + row colors feature

--- selenium-test/step-definitions/different-row-colors-steps.js
+++ selenium-test/step-definitions/different-row-colors-steps.js
@@ -5,20 +5,21 @@ module.exports = function () {
 	this.Given(/^that I’m viewing a shopping list with two or more items$/, async function() {
 		let pages = new Pages(helpers,driver,by);
 		await pages.twoItems();
-		await driver.sleep(3000);
+
+		assert(await driver.getCurrentUrl()==="http://localhost:3000/#list", "Wrong webpage loaded!");
+
+		let rows = await driver.findElements(by.css("#item-list tbody tr"));
+		assert(rows.length>1, "Not enough items");
 
 	});
 
-	this.When(/^look at the list$/, async function() {
+	this.When(/^I look at the list$/, async function() {
 
 	});
        
     this.Then(/^every second row should be in a different color$/, async function() {
-    	let trs = await driver.findElements(by.css("#collection-list tbody tr"));
-    	for(tr of trs){
-			let text = await tr.getText();
-			if(text === 'Mina matvaror'){ found = true; break; }
-		};
+    	let trs = await driver.findElements(by.css("#item-list tbody tr"));
+		assert(await trs[0].getCssValue('background-color') != await trs[1].getCssValue('background-color'), "The rows have the same background")
 	});
 
 
