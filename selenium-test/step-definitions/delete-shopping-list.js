aa94c44aaef8064a81116d72ffec8c353d67f9e8
marialjungberg <k.maria.ljungberg@gmail.com>
2017-05-30 09:27:25 +0200

    Added feature + step definitions for delete-shopping-list - not complete

---
+++ selenium-test/step-definitions/delete-shopping-list.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+	this.Given(/^that I have created a shopping list$/, function(callback) {
+	  callback.pending();
+	});
+	                        
+	this.When(/^I clicked the trash button$/, function(callback) {
+	  callback.pending();
+	});
+	                        
+	this.When(/^click the OK button on the modal$/, function(callback) {
+	  callback.pending();
+	});
+	                        
+	this.Then(/^the list should be removed from the shopping list collection$/, function(callback) {
+	  callback.pending();
+	});
+                        
+};
