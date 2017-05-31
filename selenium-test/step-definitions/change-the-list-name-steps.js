fe391e0bee5c0fab4fdedfc52616e7802ec15995
Mshkeso <msh.keso@gmail.com>
2017-05-31 12:48:38 +0200

    presentation prep with sleeps

--- selenium-test/step-definitions/change-the-list-name-steps.js
+++ selenium-test/step-definitions/change-the-list-name-steps.js
@@ -1,23 +1,25 @@
 module.exports = function () {
 
-	let Pages = require("../helper-class/load-pages.class.js");
-
 	this.Given(/^clicked the pen button$/, async function() {
+		await driver.sleep(5000);
 		await driver.findElement(by.css(".edit-list")).click();
 	});
 
 	this.When(/^I fill in the new name$/, async function() {
         let input = await driver.findElement(by.css("#edit-list-name"));
 		await driver.wait(until.elementIsVisible(input, 3000));
+		await driver.sleep(5000);
         await driver.findElement(by.css("#edit-shoppinglist-name")).sendKeys("New name");
 	});
          
 
     this.When(/^click the OK button$/, async function() {
+		await driver.sleep(5000);
 		await driver.findElement(by.css(".edit-list-modal")).click();
 	});
 
     this.Then(/^the list should be called the new name$/, async function() {
+		await driver.sleep(5000);
     	let trs = await driver.findElements(by.css("#collection-list tr"));
 		let found = false;
 		for(tr of trs){
